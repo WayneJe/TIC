d1ccdcac4ffc5ff26d76049ecbb8e3bad96246a1
unknown <wayne930518@gmail.com>
2019-05-27 17:15:23 +0800

    update 2

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -3,34 +3,41 @@ import Square from './Square'
 
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
 
 
     handleClick(i) {
-        const squares = this.state.squares.slice();
+    	const history = this.state.history.slice(0, this.state.stepNumber +1);
+    	const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+        	return;
+		}
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-			squares, squares,
+			history: history.concat([{
+            squares: squares,
+		}]),
+            stepNumber: history.length,
         	xIsNext: !this.state.xIsNext,
         });
     }
 
+    junmTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
 
 	renderSquare(i) {
 		return <Square
-			value={this.state.squares[i]}
-			onClick={() => this.handleClick(i)}
+			value={this.props.squares[i]}
+			onClick={() => this.props.handleClick(i)}
 		/>;
 	}
 
 	render() {
-		const status = 'NEXT PLAYER : ' + (this.state.xIsNext ? 'X' : 'O');
 
 		return (
 			<div>
@@ -55,5 +62,24 @@ class Board extends React.Component {
 		)
 	}
 }
-
 export default Board;
+
+function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+    ];
+    for(let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -2,18 +2,52 @@ import React,{Component} from 'react';
 import Board from "./Board";
 import './style.css'
 
-
-
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).sill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
     render() {
+        const history = this.state.history;
+        const current = history[history.state.stepNumber];
+        const winner = calculateWinner(current,squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner){
+            stutas = 'Winner: ' + winner;
+        } else {
+            status = 'Next palyer: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return(
             <div className="game">
                 <div className="game-board">
-                    <Board/>
+                    <Board
+                        squares = {current.squares}
+                        onClick = {(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-into">
-                    <div>{/*status*/}</div>
-                    <ol>{/*todo*/}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -21,3 +55,4 @@ class Game extends React.Component {
 }
 
 export default Game;
+
